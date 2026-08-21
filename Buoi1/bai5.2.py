850f3e5aa45f47bd790c5b50f6fc802f2a594e68
theanh19zz <danchoimoi34@gmail.com>
2026-08-21 23:50:38 +0700

    Update bai5.2.py

--- Buoi1/bai5.2.py
+++ Buoi1/bai5.2.py
@@ -1,1 +1,12 @@
-
+diem = 6.5
+tuoi = 20
+check_diem = (diem >= 6.5 and diem < 8.0)
+check_tuoi = (tuoi < 18 or tuoi > 60)
+phu_dinh_diem = not check_diem
+phu_dinh_tuoi = not check_tuoi
+print("Diem: ", diem)
+print("Tuoi: ", tuoi)
+print("Check diem: ", check_diem)
+print("Check tuoi: ", check_tuoi)
+print("Phu dinh diem: ", phu_dinh_diem)
+print("Phu dinh tuoi: ", phu_dinh_tuoi)
