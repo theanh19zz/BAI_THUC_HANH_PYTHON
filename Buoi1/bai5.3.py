5d4d855205b882acf0f4eeccdb343b6df13d79f6
theanh19zz <danchoimoi34@gmail.com>
2026-08-21 23:51:41 +0700

    Update bai5.3.py

--- Buoi1/bai5.3.py
+++ Buoi1/bai5.3.py
@@ -1,1 +1,20 @@
+x = 10
+x += 5
+print(x)
+x-= 3
+print(x)
+x*= 2
+print(x)
+x/= 4
+print(x)
+x//= 2
+print(x)
+x**= 3
+print(x)
 
+danh_sach = [1, 2, 3, "python"]
+print(3 in danh_sach)
+test = danh_sach
+print(test is danh_sach)
+# Dùng toán tử "in" để kiểm tra 3 có trong danh_sach không
+# Dùng toán tử "is" để so sánh 2 biến cùng tham chiếu tới 1 list
