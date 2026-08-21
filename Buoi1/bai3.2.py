a01513b7f2ea589ea039b0bf3da94d2825ebc007
theanh19zz <danchoimoi34@gmail.com>
2026-08-21 23:48:54 +0700

    Update bai3.2.py

--- Buoi1/bai3.2.py
+++ Buoi1/bai3.2.py
@@ -1,1 +1,9 @@
-
+ten = "Nguyen Van A"
+diem_toan = 8.5
+diem_van = 7.0
+so_luong_mon_hoc = 2
+MUC_LUONG_TOI_THIEU = 5000000
+print("Ten: ", ten)
+print("Diem toan: ", diem_toan)
+print("Diem van: ", diem_van)
+print("So luong mon hoc: ", so_luong_mon_hoc)
