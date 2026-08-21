450b1e4423c37e5563243b4a2dc3c6317c683c72
theanh19zz <danchoimoi34@gmail.com>
2026-08-21 23:50:04 +0700

    Update bai5.1.py

--- Buoi1/bai5.1.py
+++ Buoi1/bai5.1.py
@@ -1,1 +1,9 @@
-
+a = 17
+b = 5
+print("Tong: ", a + b)
+print("Hieu: ", a - b)
+print("Tich: ", a * b)
+print("Thuong: ", a / b)
+print("Chia lay nguyen: ", a // b)
+print("Phan du: ", a % b)
+print("Luy thua: ", a ** b)
